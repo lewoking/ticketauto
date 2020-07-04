a797251cbe8d7e567073a12803b051e1f3c650be
lewoking <lewoking@gmail.com>
2020-07-04 12:21:04 +0800

    test

---
+++ server/db.js
@@ -0,0 +1,19 @@
+// 数据库连接配置
+
+module.exports = {
+
+    mysql: {
+
+        host: 'localhost',
+
+        user: 'root',
+
+        password: 'usbw',
+
+        database: 'test',
+
+        port: '3306'
+
+    }
+
+}
